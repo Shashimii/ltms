8a91e347b355fd9a78a1d49fe29ca0feab7e175f
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-04 15:06:58 +0800

    + use the factory

--- app/Models/AssignedTask.php
+++ app/Models/AssignedTask.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssignedTask extends Model
 {
+    use HasFactory;
     protected $fillable = ['officer_id', 'duty_id', 'odts_code', 'assigned_at', 'is_done'];
     protected $casts = [
         'assigned_at' => 'date:Y-m-d',  // Format the assigned_at column as a date
