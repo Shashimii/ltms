99b76a4c57b0af21323d3116fd353cb61ada8c6f
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-05 08:25:59 +0800

    + role based layout

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,9 +1,11 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import ChiefLayout from '@/Layouts/ChiefLayout.vue';
+import OfficerLayout from '@/Layouts/OfficerLayout.vue';
 import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, usePage } from '@inertiajs/vue3';
 
 defineProps({
     mustVerifyEmail: {
@@ -13,44 +15,124 @@ defineProps({
         type: String,
     },
 });
+
+const user = usePage().props.auth.user;
 </script>
 
 <template>
     <Head title="Profile" />
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
-                Profile
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-                <div
-                    class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+    <div v-if="user.role === 2">
+        <AdminLayout>
+            <template #header>
+                <h2
+                    class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
                 >
-                    <UpdateProfileInformationForm
-                        :must-verify-email="mustVerifyEmail"
-                        :status="status"
-                        class="max-w-xl"
-                    />
-                </div>
+                    Profile
+                </h2>
+            </template>
 
-                <div
-                    class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
-                >
-                    <UpdatePasswordForm class="max-w-xl" />
-                </div>
+            <div class="py-12">
+                <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <UpdateProfileInformationForm
+                            :must-verify-email="mustVerifyEmail"
+                            :status="status"
+                            class="max-w-xl"
+                        />
+                    </div>
 
-                <div
-                    class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
-                >
-                    <DeleteUserForm class="max-w-xl" />
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <UpdatePasswordForm class="max-w-xl" />
+                    </div>
+
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <DeleteUserForm class="max-w-xl" />
+                    </div>
                 </div>
             </div>
-        </div>
-    </AuthenticatedLayout>
+        </AdminLayout>
+    </div>
+
+    <div v-if="user.role === 1">
+        <ChiefLayout>
+            <template #header>
+                <h2
+                    class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+                >
+                    Profile
+                </h2>
+            </template>
+
+            <div class="py-12">
+                <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <UpdateProfileInformationForm
+                            :must-verify-email="mustVerifyEmail"
+                            :status="status"
+                            class="max-w-xl"
+                        />
+                    </div>
+
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <UpdatePasswordForm class="max-w-xl" />
+                    </div>
+
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <DeleteUserForm class="max-w-xl" />
+                    </div>
+                </div>
+            </div>
+        </ChiefLayout>
+    </div>
+
+    <div v-if="user.role === 0">
+        <OfficerLayout>
+            <template #header>
+                <h2
+                    class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+                >
+                    Profile
+                </h2>
+            </template>
+
+            <div class="py-12">
+                <div class="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <UpdateProfileInformationForm
+                            :must-verify-email="mustVerifyEmail"
+                            :status="status"
+                            class="max-w-xl"
+                        />
+                    </div>
+
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <UpdatePasswordForm class="max-w-xl" />
+                    </div>
+
+                    <div
+                        class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
+                    >
+                        <DeleteUserForm class="max-w-xl" />
+                    </div>
+                </div>
+            </div>
+        </OfficerLayout>
+    </div>
 </template>
