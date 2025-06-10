b1be61485509e05f6a6dce61201b762c6de2dbb6
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-10 16:54:05 +0800

    + requests controller

---
+++ app/Http/Controllers/RequestListController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\AssignedTaskResource;
+use App\Models\AssignedTask;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class RequestListController extends Controller
+{
+    public function index(Request $request)
+    {   
+        $user = auth()->user();
+        
+        if ($user->role === User::ROLE_CHIEF) {
+            $requests = AssignedTaskResource::collection(AssignedTask::whereIn('request_status', [1, 2])->get());
+
+            return Inertia::render('Chief/RequestList/Index', [
+                'requests' => $requests,
+            ]);
+        }
+
+        abort(403);
+    }
+}
