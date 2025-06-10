b7a48e4ae34b14c169c1534c7ad04a5e1f9fc231
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-10 09:38:31 +0800

    # some fixes

--- resources/js/Pages/Chief/AssignedTask/Index.vue
+++ resources/js/Pages/Chief/AssignedTask/Index.vue
@@ -267,7 +267,7 @@ watch(
                             v-model="search"
                             type="text"
                             autocomplete="off"
-                            placeholder="Search duty, odts..."
+                            placeholder="Search task, odts..."
                             id="search"
                             class="block rounded-lg border-0 py-2 pl-10 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
