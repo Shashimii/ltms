eeff4ef27addefd6f1827237e6bf0b66e4d1c233
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-13 15:36:03 +0800

    + activitylog model & migration

---
+++ database/migrations/2025_06_13_071602_create_activity_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('admin_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('chief_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('officer_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->string('activity');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity_logs');
+    }
+};
