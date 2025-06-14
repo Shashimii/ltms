24e24544435418dc1ed165795584050666dba245
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-14 21:09:14 +0800

    * frontend changes

--- resources/js/Pages/Admin/User.vue
+++ resources/js/Pages/Admin/User.vue
@@ -223,7 +223,7 @@ const deleteUser = (id) => {
                             v-model="search"
                             type="text"
                             autocomplete="off"
-                            placeholder="Search task, odts..."
+                            placeholder="Search name, email..."
                             id="search"
                             class="block rounded-lg border-0 py-2 pl-10 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
