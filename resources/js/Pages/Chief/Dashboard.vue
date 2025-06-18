366102e48152da89c2b1bbdc26b8c6a40e7dab77
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-18 08:04:56 +0800

    * visual changes

--- resources/js/Pages/Chief/Dashboard.vue
+++ resources/js/Pages/Chief/Dashboard.vue
@@ -493,7 +493,7 @@ const notificationRoute = () => {
                             @click="logsRoute"
                             class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                         >
-                            View All
+                            View All Logs
                         </PrimaryButton>
                     </div>
                 </div>
