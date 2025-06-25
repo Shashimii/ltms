bae8b817a335efb370f3f1cde4fb31eedbdf1fbf
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-25 14:17:45 +0800

    * task list button changed to icons and some fixes to assigned task

--- resources/js/Pages/Chief/TaskList/Index.vue
+++ resources/js/Pages/Chief/TaskList/Index.vue
@@ -235,12 +235,16 @@ watch(
                                             <td
                                                 class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium space-x-2 sm:pr-6"
                                             >
-                                            <PrimaryButton @click="openModalFormEdit(task)" class="bg-blue-600 hover:bg-blue-500">
-                                                Edit
-                                            </PrimaryButton>
-                                            <DangerButton @click="openDeleteModal(task)">
-                                                Delete
-                                            </DangerButton>
+                                                <button @click="openModalFormEdit(task)" :disabled="editForm.processing" class="text-blue-700 hover:text-blue-400" title="Edit Task">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                    </svg>
+                                                </button>
+                                                <button @click="openDeleteModal(task)" :disabled="deleteForm.processing" class="text-red-700 hover:text-red-400" title="Delete Task">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                    </svg>
+                                                </button>
                                             </td>
                                         </tr>
                                     </tbody>
