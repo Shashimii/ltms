f7abd27c1b465c01510599aca06ac25496271e00
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-07-01 13:53:25 +0800

    + tasklist mobile

--- resources/js/Pages/Chief/TaskList/Index.vue
+++ resources/js/Pages/Chief/TaskList/Index.vue
@@ -201,79 +201,110 @@ watch(
                         <div
                             class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
                         >
-                            <div
-                                class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg relative"
-                            >
-                                <table
-                                    class="min-w-full divide-y divide-gray-300"
+                            <div class="hidden sm:block">
+                                <div
+                                    class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg relative"
                                 >
-                                    <thead class="bg-gray-50 dark:bg-gray-700">
-                                        <tr>
-                                            <th
-                                                scope="col"
-                                                class="px-3 py-3 text-left text-sm font-semibold text-gray-900 dark:text-white"
-                                            >
-                                                Task Name
-                                            </th>
-                                            <th
-                                                scope="col"
-                                                class="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                                            />
-                                        </tr>
-                                    </thead>
-                                    <tbody
-                                        v-if="tasks.data.length != 0"
-                                        class="divide-y divide-gray-200 bg-white dark:bg-gray-800"
+                                    <table
+                                        class="min-w-full divide-y divide-gray-300"
                                     >
-                                        <tr v-for="task in tasks.data" :key="task.id" class="transition duration-300 hover:bg-gray-100 dark:hover:bg-black">
-                                            <td
-                                                class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 dark:text-white"
-                                            >
-                                                {{ task.name }}
-                                            </td>
+                                        <thead class="bg-gray-50 dark:bg-gray-700">
+                                            <tr>
+                                                <th
+                                                    scope="col"
+                                                    class="px-3 py-3 text-left text-sm font-semibold text-gray-900 dark:text-white"
+                                                >
+                                                    Task Name
+                                                </th>
+                                                <th
+                                                    scope="col"
+                                                    class="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                                                />
+                                            </tr>
+                                        </thead>
+                                        <tbody
+                                            v-if="tasks.data.length != 0"
+                                            class="divide-y divide-gray-200 bg-white dark:bg-gray-800"
+                                        >
+                                            <tr v-for="task in tasks.data" :key="task.id" class="transition duration-300 hover:bg-gray-100 dark:hover:bg-black">
+                                                <td
+                                                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 dark:text-white"
+                                                >
+                                                    {{ task.name }}
+                                                </td>
 
-                                            <td
-                                                class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium space-x-2 sm:pr-6"
-                                            >
-                                                <button @click="openModalFormEdit(task)" :disabled="editForm.processing" class="text-blue-700 hover:text-blue-400" title="Edit Task">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                                    </svg>
-                                                </button>
-                                                <button @click="openDeleteModal(task)" :disabled="deleteForm.processing" class="text-red-700 hover:text-red-400" title="Delete Task">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                    </svg>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                    <tbody v-else class="divide-y divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-800">
-                                        <tr>
-                                            <td colspan="100%" class="py-12">
-                                                <div class="flex flex-col items-center justify-center text-green-700 dark:text-green-400">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke-width="1.5"
-                                                        stroke="currentColor"
-                                                        class="size-16 mb-4">
-                                                        <path stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 
-                                                            3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                                    </svg>
-                                                    <p class="text-2xl italic text-gray-600 dark:text-gray-300">No results found.</p>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                                <td
+                                                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium space-x-2 sm:pr-6"
+                                                >
+                                                    <button @click="openModalFormEdit(task)" :disabled="editForm.processing" class="text-blue-700 hover:text-blue-400" title="Edit Task">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                        </svg>
+                                                    </button>
+                                                    <button @click="openDeleteModal(task)" :disabled="deleteForm.processing" class="text-red-700 hover:text-red-400" title="Delete Task">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                        </svg>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                        <tbody v-else class="divide-y divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-800">
+                                            <tr>
+                                                <td colspan="100%" class="py-12">
+                                                    <div class="flex flex-col items-center justify-center text-green-700 dark:text-green-400">
+                                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke-width="1.5"
+                                                            stroke="currentColor"
+                                                            class="size-16 mb-4">
+                                                            <path stroke-linecap="round"
+                                                                stroke-linejoin="round"
+                                                                d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 
+                                                                3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                        </svg>
+                                                        <p class="text-2xl italic text-gray-600 dark:text-gray-300">No results found.</p>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <Pagination 
+                                    :data="tasks" 
+                                    :updatedPageNumber="updatedPageNumber"
+                                /> 
+                            </div>
+                            <div class="block sm:hidden space-y-4 px-4">
+                                <div
+                                    v-for="task in tasks.data"
+                                    :key="task.id"
+                                    class="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-lg shadow p-4"
+                                >
+                                <div class="flex justify-between items-center mb-2">
+                                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                        {{ task.name }}
+                                    </h3>
+                                </div>
+
+                                <div class="mt-4 flex gap-2">
+                                    <PrimaryButton
+                                        class="flex-1 justify-center bg-blue-600 hover:bg-blue-500 dark:bg-blue-500 dark:hover:bg-blue-400"
+                                        @click="openModalFormEdit(task)"
+                                    >
+                                        Edit
+                                    </PrimaryButton>
+                                    <DangerButton class="flex-1 justify-center" @click="openDeleteModal(task)">
+                                        Delete
+                                    </DangerButton>
+                                </div>
                             </div>
                             <Pagination 
                                 :data="tasks" 
                                 :updatedPageNumber="updatedPageNumber"
-                            /> 
+                                /> 
+                            </div>
                         </div>
                     </div>
                 </div>
