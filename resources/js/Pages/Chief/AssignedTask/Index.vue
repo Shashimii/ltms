7498bb9234fc1f20d8ec7e60e663ac565b4566fe
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-07-01 13:38:35 +0800

    # dark mode edit modal problem fix

--- resources/js/Pages/Chief/AssignedTask/Index.vue
+++ resources/js/Pages/Chief/AssignedTask/Index.vue
@@ -703,7 +703,7 @@ watch(
                             disabled
                             v-model="editForm.officer_id"
                             id="officer_id"
-                            class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:bg-green-800 sm:text-sm"
                             :class="{'text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300': editForm.errors.officer_id}"
                         >
                             <option value="">
@@ -729,7 +729,7 @@ watch(
                             disabled
                             v-model="editForm.task_id"
                             id="task_id"
-                            class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            class="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:bg-green-800 sm:text-sm"
                             :class="{'text-red-900 focus:ring-red-500 focus:border-red-500 border-red-300': editForm.errors.task_id}"
                         >
                             <option value=""> 
