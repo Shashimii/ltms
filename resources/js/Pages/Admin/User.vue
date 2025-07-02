56ad1ea0fd72575b990d76a8f1023bc0f9eb21a3
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-07-02 15:18:29 +0800

    # hide the add user button

--- resources/js/Pages/Admin/User.vue
+++ resources/js/Pages/Admin/User.vue
@@ -201,14 +201,14 @@ const deleteUser = (id) => {
                         </p>
                     </div>
 
-                    <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <!-- <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                         <PrimaryButton
                             @click="openModalFormCreate(task)"
                             class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                         >
                             Add New User
                         </PrimaryButton>
-                    </div>
+                    </div> -->
                 </div>
 
                 <div class="flex flex-col justify-left gap-2 sm:flex-row mt-6">
